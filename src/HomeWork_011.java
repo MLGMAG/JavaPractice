2dd778f74fb9856bbc343377c80b98ccb3f13dbf
MLGMAG <akhmedovmagomed2001@gmail.com>
2017-04-24 21:45:30 +0300

    HomeWork01.1

--- src/HomeWork_011.java
+++ src/HomeWork_011.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class HomeWork_011 {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter your name:");
         String name = scan.next();
         System.out.println("Hello,"+name);
