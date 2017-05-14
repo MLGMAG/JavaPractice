39e580d855b15c1d412b965e9250b4548ec726b1
MLGMAG <akhmedovmagomed2001@gmail.com>
2017-05-14 21:47:18 +0300

    Offline

---
+++ src/GoJava_Offline/Module_3/Exercise_1/Rectangle.java
@@ -0,0 +1,7 @@
+package GoJava_Offline.Module_3.Exercise_1;
+
+/**
+ * Created by Magomed on 11.05.2017.
+ */
+public class Rectangle {
+}
---
+++ src/GoJava_Offline/Module_3/Exercise_1/Test/Shape.java
@@ -0,0 +1,7 @@
+package GoJava_Offline.Module_3.Exercise_1.Test;
+
+/**
+ * Created by Magomed on 13.05.2017.
+ */
+public class Shape {
+}
