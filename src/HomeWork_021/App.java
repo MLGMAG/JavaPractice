df9ded2681572cd7ccfce0839770c072226f96bd
MLGMAG <akhmedovmagomed2001@gmail.com>
2017-05-19 20:53:00 +0300

    Fix and HomeWork_021

---
+++ src/HomeWork_021/App.java
@@ -0,0 +1,7 @@
+package HomeWork_021;
+
+/**
+ * Created by Magomed on 14.05.2017.
+ */
+public class App {
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Magomed on 14.05.2017.
+ */
+public class Test {
+}
